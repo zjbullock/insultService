38ea74b3c8228842dfaf2d22c6eb050b6595787b
Zee <zjbulllock@gmail.com>
2019-09-22 11:28:31 -0400

    began working on tests

--- repository/firestore_test.go
+++ repository/firestore_test.go
@@ -1,1 +1,43 @@
 package repository
+
+import (
+	"randomInsultService/datasource"
+	"randomInsultService/mocks"
+	"randomInsultService/model"
+	"testing"
+)
+
+func TestFireStore_InsertEntry(t *testing.T) {
+	tests := []struct {
+		name string
+		fs   fireStore
+		ds   datasource.DataSource
+		in   struct {
+			insultContent model.InsultContent
+		}
+		expected struct {
+			id  *string
+			err error
+		}
+	}{
+		{
+			name: "error initializing data source",
+			fs:   fireStore{},
+			ds:   &mocks.DataSource{},
+			in: struct {
+				insultContent model.InsultContent
+			}{
+				insultContent: model.InsultContent{
+					Verb:      "test verb",
+					Adjective: "test adjective",
+					Noun:      "test noun ",
+				},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+		})
+	}
+}
