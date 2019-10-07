173122b3ff1a039b4174ef8c0729be5a1d449c17
Zee <zjbulllock@gmail.com>
2019-10-07 00:05:53 -0400

    Added ability for service to text insults

---
+++ service/helper/helper.go
@@ -0,0 +1,31 @@
+package helper
+
+import (
+	"encoding/json"
+	"github.com/juju/loggo"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"os"
+	"randomInsultService/model"
+)
+
+func GetSecrets(log loggo.Logger) (*model.Secrets, error) {
+	file, err := os.Open("./secrets/secrets.json")
+	if err != nil {
+		return nil, errors.Wrapf(err, "error getting twilio stuff")
+	}
+	defer file.Close()
+	log.Infof("Helper - GetSecrets - Reading File")
+	byteValue, _ := ioutil.ReadAll(file)
+
+	log.Infof("Helper - GetSecrets - Now unmarshalling secrets")
+
+	var secret model.Secrets
+	err = json.Unmarshal(byteValue, &secret)
+	if err != nil {
+		return nil, errors.Wrap(err, "error unmarshaling json")
+	}
+	log.Infof("Helper - GetSecrets - Now returning with secrets")
+
+	return &secret, nil
+}
