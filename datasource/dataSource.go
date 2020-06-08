12d2f11a7ceb8683b30ff9fd05c98fc7a7858d81
Zee <zjbulllock@gmail.com>
2020-06-08 04:06:07 -0400

    added leveling feature

--- datasource/dataSource.go
+++ datasource/dataSource.go
@@ -4,16 +4,20 @@ import (
 	. "cloud.google.com/go/firestore"
 	"context"
 	"github.com/juju/loggo"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
+	"insultService/model"
 )
 
 // DataSource defines what functions we need from the database
 type DataSource interface {
 	OpenConnection() error
 	CloseConnection() error
-	ReadEntries() (*DocumentSnapshot, error)
+	ReadEntries(collection, doc string) (*DocumentSnapshot, error)
 	InsertEntry(content interface{}, collection string) (*DocumentRef, *WriteResult, error)
 	ReadCollection(collection string) *DocumentIterator
+	QueryCollection(collection string, args []model.QueryArg) ([]*DocumentSnapshot, error)
+	UpdateDocument(collection, doc string, content interface{}) error
 }
 
 type fireStoreDB struct {
@@ -57,8 +61,9 @@ func (f *fireStoreDB) InsertEntry(content interface{}, collection string) (*Docu
 	}
 	return d, wr, nil
 }
-func (f *fireStoreDB) ReadEntries() (*DocumentSnapshot, error) {
-	words, err := f.client.Collection("words").Doc("list").Get(f.ctx)
+
+func (f *fireStoreDB) ReadEntries(collection, doc string) (*DocumentSnapshot, error) {
+	words, err := f.client.Collection(collection).Doc(doc).Get(f.ctx)
 	if err != nil {
 		f.log.Errorf("failed to read possible words with error: %v", err)
 		return nil, err
@@ -78,5 +83,41 @@ func (f *fireStoreDB) CloseConnection() error {
 		return err
 	}
 	f.client = nil
+	return nil
+}
+
+func (f *fireStoreDB) QueryCollection(collection string, args []model.QueryArg) ([]*DocumentSnapshot, error) {
+	q := f.client.Collection(collection).Query
+	for _, arg := range args {
+		q = q.Where(arg.Path, arg.Op, arg.Value)
+	}
+	iter := q.Documents(f.ctx)
+	defer iter.Stop()
+	var docs []*DocumentSnapshot
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			f.log.Errorf("error iterating through queried documents with error: %v", err)
+			return nil, err
+		}
+		docs = append(docs, doc)
+	}
+	return docs, nil
+}
+
+func (f *fireStoreDB) UpdateDocument(collection, doc string, content interface{}) error {
+	f.log.Infof("collection name: %s", collection)
+	col := f.client.Collection(collection)
+	f.log.Infof("collection: %v", col)
+	//Should insert generated insult into firestore
+	_, err := col.Doc(doc).Set(f.ctx, content)
+	if err != nil {
+		//Should allow error to bubble up upon failure
+		f.log.Errorf("failed to update doc %v with content: %v for write: %v", doc, content, err)
+		return err
+	}
 	return nil
 }
--- repository/firestore.go
+++ repository/firestore.go
@@ -14,6 +14,9 @@ type FireStore interface {
 	ReadAllWords() (*model.Words, error)
 	InsertInsultEntry(insultContent model.InsultContent) (*string, error)
 	ReadInsults() ([]model.InsultContent, error)
+	ReadUserInfo(args []model.QueryArg) ([]*model.UserInfo, error)
+	ReadTitles() (*model.Titles, error)
+	UpdateUserInfo(info *model.UserInfo) error
 }
 
 type fireStore struct {
@@ -36,7 +39,7 @@ func (f *fireStore) ReadAllWords() (*model.Words, error) {
 		return nil, errors.New(fmt.Sprintf("error initializing data source: %v", err))
 	}
 	defer f.ds.CloseConnection()
-	doc, err := f.ds.ReadEntries()
+	doc, err := f.ds.ReadEntries("words", "list")
 	if err != nil {
 		f.log.Errorf("error reading from datasource: %v", err)
 		return nil, errors.New(fmt.Sprintf("error reading from datasource: %v", err))
@@ -58,7 +61,7 @@ func (f *fireStore) InsertInsultEntry(insultContent model.InsultContent) (*strin
 		return nil, errors.New(fmt.Sprintf("error Intializing data source: %v", err))
 	}
 	defer f.ds.CloseConnection()
-	d, _, err := f.ds.InsertEntry(insultContent, "insult")
+	d, _, err := f.ds.InsertEntry(insultContent, "insults")
 	if err != nil {
 		//Should allow error to bubble up upon failure
 		f.log.Errorf("failed to create an insult doc for write: %v", err)
@@ -74,7 +77,7 @@ func (f *fireStore) ReadInsults() (insultContents []model.InsultContent, err err
 		return nil, errors.New(fmt.Sprintf("error Intializing data source: %v", err))
 	}
 	defer f.ds.CloseConnection()
-	d := f.ds.ReadCollection("insults")
+	d := f.ds.ReadCollection("titles")
 	defer d.Stop()
 	for {
 		doc, err := d.Next()
@@ -82,14 +85,82 @@ func (f *fireStore) ReadInsults() (insultContents []model.InsultContent, err err
 			break
 		}
 		if err != nil {
-			f.log.Errorf("failed to read document insult contents: %v", err)
+			f.log.Errorf("failed to read document titles contents: %v", err)
 		}
 		var insultContent model.InsultContent
 		if err := doc.DataTo(&insultContent); err != nil {
-			f.log.Errorf("error converting document snap to an insult content model")
+			f.log.Errorf("error converting document snap to an titles model")
 			return nil, errors.New(fmt.Sprintf("error converting document snap to a model.InsultContent, :%v", err))
 		}
 		insultContents = append(insultContents, insultContent)
 	}
 	return insultContents, nil
+}
+
+func (f *fireStore) ReadTitles() (*model.Titles, error) {
+	err := f.ds.OpenConnection()
+	if err != nil {
+		f.log.Errorf("error initializing datasource: %v", err)
+		return nil, errors.New(fmt.Sprintf("error initializing data source: %v", err))
+	}
+	defer f.ds.CloseConnection()
+	doc, err := f.ds.ReadEntries("titles", "8tVQvvt1eE1Fg38fB7qH")
+	if err != nil {
+		f.log.Errorf("error reading from datasource: %v", err)
+		return nil, errors.New(fmt.Sprintf("error reading from datasource: %v", err))
+	}
+	var titles model.Titles
+	if err := doc.DataTo(&titles); err != nil {
+		f.log.Errorf("error converting document snap to a titles model")
+		return nil, errors.New(fmt.Sprintf("error converting document snap to a model.Words, :%v", err))
+	}
+	return &titles, nil
+}
+
+func (f *fireStore) ReadUserInfo(args []model.QueryArg) ([]*model.UserInfo, error) {
+	err := f.ds.OpenConnection()
+	if err != nil {
+		f.log.Errorf("failed to open datasource connection")
+		return nil, err
+	}
+	docs, err := f.ds.QueryCollection("users", args)
+	if err != nil {
+		f.log.Errorf("error querying for documents with error: %v", err)
+		return nil, err
+	}
+	var userInfo []*model.UserInfo
+	for _, doc := range docs {
+		user := model.UserInfo{}
+		err := doc.DataTo(&user)
+		if err != nil {
+			f.log.Errorf("error converting doc to profile with error: %v", err)
+			return nil, err
+		}
+		user.DocID = &doc.Ref.ID
+		userInfo = append(userInfo, &user)
+	}
+	err = f.ds.CloseConnection()
+	if err != nil {
+		f.log.Errorf("failed to close datasource connection")
+		return userInfo, err
+	}
+	return userInfo, nil
+}
+
+func (f *fireStore) UpdateUserInfo(info *model.UserInfo) error {
+	err := f.ds.OpenConnection()
+	if err != nil {
+		f.log.Errorf("failed to open datasource connection")
+		return err
+	}
+	err = f.ds.UpdateDocument("users", info.Name, info)
+	if err != nil {
+		return fmt.Errorf("failed to create a userInfo: %v", err)
+	}
+	err = f.ds.CloseConnection()
+	if err != nil {
+		f.log.Errorf("failed to close datasource connection")
+		return err
+	}
+	return nil
 }
